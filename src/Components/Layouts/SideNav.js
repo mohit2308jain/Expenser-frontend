8bcc70ee83c97f4195fdf72d1f54ac8e65d9e224
mohit2308jain <mohitjain5000@gmail.com>
2020-09-27 18:14:49 +0530

    Update

--- src/Components/Layouts/SideNav.js
+++ src/Components/Layouts/SideNav.js
@@ -50,7 +50,7 @@ const SideNav = () => {
                 </li>
 
                 <li className="nav-item">
-                    <div onClick={() => window.open('https://mohitjain.now.sh', "_blank")} className="nav-link">
+                    <div onClick={() => window.open('https://mohit-jain.vercel.app', "_blank")} className="nav-link">
                         <i className="fa fa-heart" />
                     <span className="link-text">Mohit Jain</span>
                     </div>
